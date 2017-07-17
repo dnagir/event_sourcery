fb16b53ba316147b6ea1fa66a963cbe51ebf2a93
Jiexin Huang <jiexin.huang@envato.com>
2017-07-17 14:27:20 +1000

    fix spec

--- spec/event_sourcery/event_store/event_source_spec.rb
+++ spec/event_sourcery/event_store/event_source_spec.rb
@@ -1,10 +1,8 @@
 RSpec.describe EventSourcery::EventStore::EventSource do
   let(:event_store) { double(:event_store) }
-  subject(:event_source) { described_class.new(adapter) }
+  subject(:event_source) { described_class.new(event_store) }
 
   describe 'adapter delegations' do
-    let(:adapter) { double }
-
     %w[
       get_next_from
       latest_event_id
@@ -14,7 +12,7 @@ RSpec.describe EventSourcery::EventStore::EventSource do
     ].each do |method|
       it "delegates ##{method} to the adapter" do
         allow(event_store).to receive(method.to_sym).and_return([])
-        event_store.send(method.to_sym)
+        event_source.send(method.to_sym)
         expect(event_store).to have_received(method.to_sym)
       end
     end
